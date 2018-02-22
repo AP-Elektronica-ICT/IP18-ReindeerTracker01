bc830dfe81a11be8399e799d05d819d7426fb8a4
Tom Knaepkens <tom.knaepkens@hotmail.com>
2018-02-22 12:58:45 +0100

    Added fixed project

---
+++ ReindeerTracker/ReindeerTracker/ReindeerTrackerPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ReindeerTracker
+{
+    public partial class ReindeerTrackerPage : ContentPage
+    {
+        public ReindeerTrackerPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
