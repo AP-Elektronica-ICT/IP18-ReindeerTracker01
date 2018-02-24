8e7889ebfa4cab495bfe0e0a6de0b4f2c57859d1
Tom Knaepkens <tom.knaepkens@hotmail.com>
2018-02-24 16:44:14 +0100

    Added Reindeer list with dummy data

--- ReindeerTracker/src/pages/home/home.ts
+++ ReindeerTracker/src/pages/home/home.ts
@@ -8,7 +8,33 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+ 
   }
 
+  data: IReindeer[] = 
+  [
+    {
+      "id": 1,
+      "activity":  new Date(2018,2,23,14,2),
+      "status": true,
+      "battery": 75
+    },
+    {
+      "id": 2,
+      "activity":  new Date(2017,2,23,4,2),
+      "status": false,
+      "battery": 35
+    }
+  ]
+
+
+
+}
+
+interface IReindeer
+{
+id : number;
+activity : Date;
+status : boolean;
+battery : number;
 }
