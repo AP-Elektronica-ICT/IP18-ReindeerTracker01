fb73102cf530b5396530f65b6cf74d2c5fc4f1ba
Unknown <yorick.scheyltjens@gmail.com>
2018-02-27 14:21:16 +0100

    Add Server

---
+++ Server/API/data/view.php
@@ -0,0 +1,31 @@
+<?php
+
+
+include "../db_login.php";
+
+
+$query = "select * from data";
+$result = mysql_query($query);
+
+echo 
+"<table><thead>
+<th>Serial Number</th>
+<th>Latitude</th>
+<th>Longitude</th>
+<th>Status</th>
+<th>Battery</th>
+<th>Time</th></thead><tbody>";
+while($row = mysql_fetch_assoc($result))
+{
+    $serialnumber = $row['serialnumber'];
+    $lat = $row['latitude'];
+    $long = $row['longitude'];
+    $status = $row['status'];
+    $battery = $row['battery'];
+    $time = $row['time'];
+    echo "<tr><td>$serialnumber</td><td>$lat</td><td>$long</td><td>$status</td><td>$battery</td><td>$time</td></tr>";
+}
+echo "</tbody></table>";
+
+
+?>
---
+++ Server/API/reindeer/index.php
@@ -0,0 +1,46 @@
+<?php
+    include "../db_login.php";
+    $reindeerId = $_GET['reindeerId'];
+    $limit = $_GET['limit'];
+    header('Access-Control-Allow-Origin: *'); 
+    $query = "select * from reindeer where id = '$reindeerId'";
+    $result = mysql_query($query);
+    $json = array();
+    while($row = mysql_fetch_assoc($result))
+    {
+        $serialnumber  = $row['serialnumber'];
+        $name = $row['name'];
+        $age = $row['age'];
+        $status = "";
+        $time = "";
+        $battery = "";
+        $lat = "";
+        $long = "";
+
+        $query = "select * from data where serialnumber = '$serialnumber' order by time DESC LIMIT 1;";
+        $result = mysql_query($query);
+        while($row = mysql_fetch_assoc($result))
+        {
+            $status = $row['status'];
+            $battery = $row['battery'];
+            $time = $row['time'];
+            $lat = $row['latitude'];
+            $long = $row['longitude'];
+        }
+        
+        $data = array();
+        $query = "select * from data where serialnumber = '$serialnumber' LIMIT $limit";
+        $result = mysql_query($query);
+        while($row = mysql_fetch_assoc($result))
+        {
+            $lat = $row['latitude'];
+            $long = $row['longitude'];
+            $array = array("lat"=>$lat,"long"=>$long);
+            array_push($data,$array);
+        }
+        $array = array("serialnumber"=>$serialnumber,"name"=>$name,"age"=>$age, "status"=>$status, "battery"=>$battery,"time"=>$time,"lat"=>$lat,"long"=>$long,"locations"=>$data );
+        array_push($json, $array);
+    }
+    echo json_encode($json);
+
+?>
