93b24effab9cab05269d62fcbb7bfbc989744464
Unknown <yorick.scheyltjens@gmail.com>
2018-02-27 16:09:42 +0100

    Fixed JSON from detail page

--- Server/API/trackers/list/index.php
+++ Server/API/trackers/list/index.php
@@ -10,7 +10,7 @@
         $serialnumber = $row['serialnumber'];
         $assigned = "";
         $query2 = "select * from reindeer where serialnumber = '$serialnumber';";
-        $result2 = mysql_query($result2);
+        $result2 = mysql_query($query2);
         if(mysql_num_rows($result2) > 0)
         {
             $assigned = "true";
