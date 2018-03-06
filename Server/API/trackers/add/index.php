ba99af2f0b44a0afd4356b9efefa16a2cf2b9b3c
Unknown <yorick.scheyltjens@gmail.com>
2018-03-06 11:44:55 +0100

    Added POST API for adding a tracker

---
+++ Server/API/trackers/add/index.php
@@ -0,0 +1,36 @@
+<?php
+
+include $_SERVER["DOCUMENT_ROOT"]."/Reindeertracker/API/db_login.php";
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
+if(isset($_POST))
+{
+    $inputJSON = file_get_contents('php://input');
+    $test = json_decode($inputJSON, true);
+    $data = json_decode($test, true);
+
+    
+    $serialnumber = $data['serialnumber'];
+    $userId = $data['userId'];
+
+    
+
+    $sql = "update  devices set userId = $userId where serialnumber =$serialnumber;";
+    
+    if(!mysql_query($sql,$con))
+    {
+        die('Error : ' . mysql_error());
+    }
+    else
+    {
+        echo "true";
+    }
+
+
+    
+    
+
+}
+?>
