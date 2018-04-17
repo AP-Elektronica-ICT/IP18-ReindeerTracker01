3a2a5feb0b600f3e438bf77faa6c96e41beda749
Unknown <yorick.scheyltjens@gmail.com>
2018-04-17 18:01:48 +0200

    Updated detail API

--- Server/API/reindeer/detail/index.php
+++ Server/API/reindeer/detail/index.php
@@ -3,6 +3,15 @@
     $reindeerId = $_GET['reindeerId'];
     $limit = $_GET['limit'];
     header('Access-Control-Allow-Origin: *'); 
+    $hash = $_GET['hash'];
+    $userId = 0;
+    $query = "select id from users where hash = '$hash';";
+    $result = mysql_query($query);
+    while($row = mysql_fetch_assoc($result))
+    {
+        $userId = $row['id'];
+    }
+
     $query = "select * from reindeer where id = '$reindeerId'";
     $result = mysql_query($query);
     $json = array();
@@ -16,12 +25,7 @@
         $status = "";
         $time = "";
         $battery = "";
-        $averageDistance = "";
-        $previousLocationLat = "";
-        $previousLocationLong = "";
-
-        $firstLocationLat = "";
-        $firstLocationLong = "";
+       
 
         $query = "select * from data where serialnumber = '$serialnumber' order by time ASC LIMIT 1;";
        
@@ -45,7 +49,6 @@
             $previousLocationLong = $row['longitude'];
         }
 
-        $list = array();
         $data = array();
         $query = "select * from data where serialnumber = '$serialnumber' order by time DESC LIMIT $limit";
         $result = mysql_query($query);
@@ -54,39 +57,17 @@
             $lat = $row['latitude'];
             $long = $row['longitude'];
             $tijd = $row['time'];
-            if($lat != $previousLocationLat && $long != $previousLocationLong)
-            {
-                array_push($list, intval(distance($previousLocationLat, $previousLocationLong, $lat, $long, "K")));
-            }
-            $previousLocationLat = $lat;
-            $previousLocationLong = $long;
+           
             $array = array("lat"=>$lat,"long"=>$long,"time"=>$tijd);
             array_push($data,$array);
-        }
-
+        } 
         
         
-        $array = array("reindeerId"=>$id,"serialnumber"=>$serialnumber,"name"=>$name,"birthDate"=>$birthDate,"firstLocationLat"=>$firstLocationLat, "firstLocationLong"=>$firstLocationLong, "status"=>$status, "battery"=>$battery,"time"=>$time, "averageDistance"=>array_sum($list)/count($list),"locations"=>$data);
+        $array = array("reindeerId"=>$id,"serialnumber"=>$serialnumber,"name"=>$name,"birthDate"=>$birthDate,"firstLocationLat"=>$firstLocationLat, "firstLocationLong"=>$firstLocationLong, "status"=>$status, "battery"=>$battery,"time"=>$time, "locations"=>$data);
         array_push($json, $array);
     }
     echo json_encode($json);
 
 
-    function distance($lat1, $lon1, $lat2, $lon2, $unit) {
-        $theta = $lon1 - $lon2;
-        $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
-        $dist = acos($dist);
-        $dist = rad2deg($dist);
-        $miles = $dist * 60 * 1.1515;
-        $unit = strtoupper($unit);
-      
-        if ($unit == "K") {
-            return ($miles * 1.609344);
-        } else if ($unit == "N") {
-            return ($miles * 0.8684);
-        } else {
-            return $miles;
-        }
-      }
-
+    
 ?>
